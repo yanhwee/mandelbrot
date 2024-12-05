e3762d29f50a44ef393d09af013394c6e2ac7ebb
Yan Hwee <yanhweeyee@gmail.com>
2024-12-05 12:52:38 +0800

    Add animations

--- b1.py
+++ b1.py
@@ -16,8 +16,9 @@ def escapes_(n, z, xs, ys, dtype=torch.complex64):
     cs = torch.tensor(cs, dtype=dtype, device=DEVICE)
     zs = torch.full_like(cs, z, dtype=dtype, device=DEVICE)
     zss = accumulate(repeat(cs, n), step_, initial=zs)
-    return (zs.abs().cpu().reshape(len(ys), len(xs))
-            for zs in zss)
+    ms = torch.zeros_like(cs, dtype=torch.int32, device=DEVICE)
+    mss = accumulate((zs.abs() < 2 for zs in zss), initial=ms)
+    return (ms.cpu().reshape(len(ys), len(xs)) for ms in mss)
 
 if __name__ == '__main__':
     x, y = -1, 0
@@ -28,8 +29,8 @@ if __name__ == '__main__':
     xs, ys = ws + x, ws + y
     extent = (xs[0]-hp, xs[-1]+hp, ys[0]-hp, ys[-1]+hp)
 
-    l = 65
-    arrs = escapes_(l-1, 0, xs, ys)
+    l = 100
+    arrs = escapes_(l-2, 0, xs, ys)
     frames = ((i, arr + 1) for i, arr in enumerate(arrs))
     fig, ax = plt.subplots()
     im = None
@@ -40,12 +41,13 @@ if __name__ == '__main__':
             im = ax.imshow(arr, extent=extent,
                            norm='log',
                            vmin=1,
-                           vmax=1e5)
+                           vmax=l)
             fig.colorbar(im, ax=ax)
         else:
             im.set_data(arr)
-            # im.autoscale()
-        fig.suptitle(f'magnitude (iteration {i})')
-    ani = FuncAnimation(fig, update, frames, save_count=l)
-    plt.show()
-    # ani.save('animation.mp4', 'ffmpeg', fps=2)
+            im.autoscale()
+        fig.suptitle(f'iteration {i}')
+    ani = FuncAnimation(fig, update, frames, save_count=l,
+                        interval=100)
+    # plt.show()
+    ani.save('animation.png', 'pillow', fps=8)
